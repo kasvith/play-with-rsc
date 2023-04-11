3dea8ec35e212f9c58940248db599ff968f91072
Kasun Vithanage <alankasun@gmail.com>
2023-04-12 04:14:41 +0530

    fix: ssr data fetching

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -1,9 +1,14 @@
 import ky from 'ky'
+import axios from 'axios'
 
 let cache = new Map();
 
 export function fetchData(url: string) {
   if (!cache.has(url)) {
+    if (import.meta.env.SSR) {
+      cache.set(url, axios.get(url).then((res: any) => res.data));
+      return cache.get(url);
+    }
     cache.set(url, ky(url).then((res: any) => res.json()));
   }
   return cache.get(url);
